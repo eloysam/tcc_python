76a5664260068b11f83312acb53bb48c774256cd
eloysam <114229411+eloysam@users.noreply.github.com>
2023-12-12 16:42:56 -0300

    alteracoes

--- exercicio_manipulacaoString.py
+++ exercicio_manipulacaoString.py
@@ -1,9 +1,12 @@
 
-# ********************** Exercício 01 *************************************************
+print('\tEXERCÍCIO 01')
 
-nome = input('Insira o nome da espécie:')
+nome = str(input('Insira o nome da espécie:'))
+capitalize = nome.capitalize()
 
-n = nome[0]
-print(n,'.',nome[5:])
-
+comeco = capitalize.find(capitalize[1]) # aqui eu estou pegando a segunda letra
+fim = capitalize.find(' ') # aqui eu estou pegando a parte que termina a palavra, ou seja até o espaço com a outra palavra
+substituicao = capitalize[comeco:fim]
+abreviacao = capitalize.replace(substituicao,'.')
+print('O nome', nome,'abreveado é', abreviacao)
 
