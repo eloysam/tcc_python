109d6a65511fe46eb30269f3fd7341546437880c
eloysam <114229411+eloysam@users.noreply.github.com>
2023-12-12 16:55:26 -0300

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -9,16 +9,24 @@ proteina = 'MGTAHDBSJVFMSBDBFKSLDB'
 proteinaMutante = 'DNFKJSJBFJSBVNRUGHGDKLSNFUOZN'
 
 # letra A
+
+tam1 = len(proteina)
+tam2 = len(proteinaMutante)
+
+razao = ((tam2/tam1)- 1)*100
+print('O comprimento da proteina 1 é de:', len(proteina), 'aas')
+print('O comprimento da proteina 2 é de:', len(proteinaMutante), 'aas')
+
+
+
+
+"""
 if len(proteinaMutante) > len(proteina):
     valor = float((len(proteinaMutante) - len(proteina)))/100
     print('A proteína mutante é maior:',valor,'%')
 else:
     print('Tente novamente! Obrigada')
 
+"""
 
-# letra B
-print(len(proteina))
-print(len(proteinaMutante))
-
-# letra C
 
