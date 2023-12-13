c7969e964da00f2401724a50d7465e75130accc0
eloysam <114229411+eloysam@users.noreply.github.com>
2023-12-13 10:54:23 -0300

    Create operadores.py

---
+++ operadores_condicionais/operadores.py
@@ -0,0 +1,10 @@
+# No python existe um conceito de identação
+
+if 5 > 9:
+    print('5 é maior que 3')
+print('teste') # aqui está fora do IF
+
+if 5 > 6:
+    print('5 é maior')
+else:
+    print('6 é maior')
