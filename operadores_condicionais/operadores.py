f8a889edbfb05fef84de180444fe6df6df8ceadb
eloysam <114229411+eloysam@users.noreply.github.com>
2023-12-14 09:20:28 -0300

    Update operadores.py

--- operadores_condicionais/operadores.py
+++ operadores_condicionais/operadores.py
@@ -8,3 +8,13 @@ if 5 > 6:
     print('5 é maior')
 else:
     print('6 é maior')
+
+n = 3
+
+if n == 4:
+    print('n = 4')
+else:
+    if n == 3:
+        print('n é igual a 3')
+    else:
+        print('n != 3 && n!= 4')
