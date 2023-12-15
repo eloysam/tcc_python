61d54eeb400a30e6251540b440f6360d4cae02e2
eloysam <114229411+eloysam@users.noreply.github.com>
2023-12-15 10:37:51 -0300

    Update exercicio1.py

--- operadores_condicionais/exercicio1.py
+++ operadores_condicionais/exercicio1.py
@@ -3,6 +3,7 @@
 nome_plantas = str(input('Digite o nome de uma família de plantas:'))
 nome_animais = str(input('Digite o nome de uma família de animais:'))
 
+# poderíamos usar o comando IN 
 ini = nome_plantas.find('ace')
 fim = nome_plantas.find(' ')
 P_nome = nome_plantas[ini:fim]
@@ -10,9 +11,10 @@ ini2 = nome_animais.find('ida')
 fim2 = nome_plantas.find(' ')
 A_nome = nome_animais[ini2:fim2]
 
+# else if == elif
 if nome_plantas[ini:fim] == 'acea':
     print(nome_plantas, 'pertence á Família de plantas!')
-if nome_animais[ini2:fim2]:
+elif nome_animais[ini2:fim2]:
     print(nome_animais, 'pertence á Família dos animais!')
 else:
     print('Táxon não-identificado')
