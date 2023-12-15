61580a3d36701b5d411ac74be82e7649bfa7c12d
eloysam <114229411+eloysam@users.noreply.github.com>
2023-12-15 10:43:30 -0300

    mudancas

--- operadores_condicionais/exercicio2.py
+++ operadores_condicionais/exercicio2.py
@@ -1,15 +1,15 @@
     # ANALISANDO PARÂMETROS HEMATOLÓGICOS
 
-hemacias = float(input('Digite o valor das hemácias:'))
-hemoglobina = float(input('Digite o valor da hemoglobina:'))
-hematocrito = float(input('Digite o valor do hematócrito:'))
+hemacias = float(input('Digite o valor das hemácias (x10^12/L):'))
+hemoglobina = float(input('Digite o valor da hemoglobina (g/dL):'))
+hematocrito = float(input('Digite o valor do hematócrito (%):'))
 
 if hemacias > 4.5 and hemacias < 5.5:
     print('As hemácias estão normais!')
 else:
     if hemacias < 4.5:
         print('As hemácias estão baixas!')
-    if hemacias > 5.5:
+    elif hemacias > 5.5:
         print('As hemácias estão altas!')
 
 if hemoglobina > 13 and hemoglobina < 17:
@@ -17,7 +17,7 @@ if hemoglobina > 13 and hemoglobina < 17:
 else:
     if hemoglobina < 13:
         print('As hemoglobinas estão baixas!')
-    if hemoglobina > 17:
+    elif hemoglobina > 17:
         print('As hemoglobinas estão altas!')
 
 if hematocrito > 40 and hematocrito < 50:
@@ -25,7 +25,7 @@ if hematocrito > 40 and hematocrito < 50:
 else:
     if hematocrito < 40:
         print('Os hematócritos estão baixos!')
-    if hematocrito > 50:
+    elif hematocrito > 50:
         print('Os hematócritos estão altos!')
 
 
