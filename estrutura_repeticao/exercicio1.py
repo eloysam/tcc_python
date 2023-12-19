7c7459bd5d18f00874acd60c1aaa1d47e720e0b0
eloysam <114229411+eloysam@users.noreply.github.com>
2023-12-19 10:59:52 -0300

    Create exercicio1.py

---
+++ estrutura_repeticao/exercicio1.py
@@ -0,0 +1,17 @@
+
+# Transcrição de sequência de DNA
+
+dna = str(input('Escreva uma sequência de DNA:'))
+
+rna = ''
+for nucleotideo in dna:
+    if nucleotideo == 'A':
+        rna = rna = 'U'
+    elif nucleotideo == 'T':
+        rna = rna + 'A'
+    elif nucleotideo == 'C':
+        rna = rna + 'G'
+    elif nucleotideo == 'G':
+        rna = rna + 'C'
+
+print(f'A sequência de RNA correspondente ao DNA é: {rna}')
